be104ee51ff24ed6d32d2e2971499f73e0f13a5c
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2023-07-28 22:33:30 +0530

    Time: 2 ms (47.9%), Space: 8.3 MB (37.53%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       int n=nums.size();
+       for(int i=0;i<n;i++)
+       {
+           for(int j=0;j<n;j++)
+           {
+               if(nums[i]<=nums[j])
+               {
+                   swap(nums[i],nums[j]);
+               }
+           }
+       }
+    }
+};
