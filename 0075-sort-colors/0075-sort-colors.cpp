3b7c903c8f6da5ed51d40b4dd41fd7eb82048d72
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2023-09-14 23:53:41 +0530

    Time: 0 ms (100%), Space: 8.8 MB (13.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-       int n=nums.size();
-       for(int i=0;i<n;i++)
-       {
-           for(int j=0;j<n;j++)
-           {
-               if(nums[i]<=nums[j])
-               {
-                   swap(nums[i],nums[j]);
-               }
-           }
-       }
+    void sortColors(vector<int>& arr) {
+        //We can use merge sort but it will take extra spaces.
+        int c1=0;
+        int c2=0;
+        int c3=0;
+        int n=arr.size();
+        //count how many 0's
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0) c1++;
+        }
+        //count how many 1's
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==1)  c2++;
+        }
+        //count how many 2's
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==2)  c3++;
+        }
+
+        for(int i=0;i<c1;i++)
+        {
+            arr[i]=0;
+        }
+        for(int i=c1;i<c1+c2;i++)
+        {
+            arr[i]=1;
+        }
+        for(int i=c1+c2;i<n;i++)
+        {
+            arr[i]=2;
+        }
+
     }
 };
