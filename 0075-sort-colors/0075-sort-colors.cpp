c4f8c4ada41636b8ee5758554094e02b3175b4d8
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2023-09-16 18:14:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (15.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& arr) {
-        //We can use merge sort but it will take extra spaces.
-        //Count how many 0's ,1's and 2.
-        int c1=0;
-        int c2=0;
-        int c3=0;
-        int n=arr.size();
-        //count how many 0's
-        for(int i=0;i<n;i++)
+        int low=0,mid=0,high=arr.size()-1;
+        while(mid<=high)
         {
-            if(arr[i]==0) c1++;
-        }
-        //count how many 1's
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==1)  c2++;
-        }
-        //count how many 2's
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==2)  c3++;
-        }
+            if(arr[mid]==0)
+            {
+                swap(arr[low],arr[mid]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(arr[mid],arr[high]);
+                high--;
+            }
 
-        for(int i=0;i<c1;i++)
-        {
-            arr[i]=0;
         }
-        for(int i=c1;i<c1+c2;i++)
-        {
-            arr[i]=1;
-        }
-        for(int i=c1+c2;i<n;i++)
-        {
-            arr[i]=2;
-        }
-
     }
 };
