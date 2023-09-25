2efdbdf7fd400826aa441e8a2f98c352c169943d
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2023-09-26 01:21:13 +0530

    Time: 16 ms (70.72%), Space: 19.5 MB (18.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums){
-    vector<int>temp;
-    //Step 1 : Put the non zero element in to temp. 
-    for(int i=0;i<nums.size();i++)
-    {
-        if(nums[i]!=0)
+        int i=0;
+        for(int j=0;j<nums.size();j++)
         {
-            temp.push_back(nums[i]);
+            if(nums[j]!=0)
+            {
+                swap(nums[j],nums[i]);
+                i++;
+            }
         }
-    }
-    //Step 2: Put the temp array into main array
-    for(int i=0;i<temp.size();i++)
-    {
-        nums[i]=temp[i];
-    }
-    //Step 3 :Store Zero in Remaining place in that array.
-    for(int i=temp.size();i<nums.size();i++)
-    {
-        nums[i]=0;
-    } 
     }
 };
