661a85baf2da7e425b9728b260a468afafecf8da
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2023-11-04 13:43:30 +0530

    Time: 16 ms (44.09%), Space: 18.8 MB (5.36%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,20 +3,18 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
 
+        vector<int>hash(n+1,0);      
+        for(int i=0;i<n;i++)
+        {
+            hash[nums[i]]=1;
+        }
         for(int i=0;i<=n;i++)
         {
-            int flag=0;
-            for(int j=0;j<nums.size();j++)
+            if(hash[i]==0)
             {
-                if(nums[j]==i)
-                {
-                    flag=1;
-                    break;
-                }
-            }
-            if(flag==0)
                 return i;
-        }   
-    return -1;  
+            }
+        }
+    return -1;
     }
 };
