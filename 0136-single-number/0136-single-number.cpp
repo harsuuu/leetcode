eba4f8e5ee148cec32d0c86671183df83bc7a464
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2023-11-05 12:01:08 +0530

    Time: 20 ms (28.99%), Space: 16.9 MB (97.07%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    //if array is [2,2,1] then output is 1.
-    //if we will do xor operation of all element then it will be like 2^2^1 then result will 0^1 and zero ^ Number == Number .
-    int xorr = 0;
-    for( int i=0; i< nums.size(); i++ )
-    {
-        xorr = xorr^nums[i];
-    }
-    return xorr;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                return nums[i-1];
+            }
+        }return nums[nums.size()-1];
     }
 };
