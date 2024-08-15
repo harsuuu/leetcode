aa88826ab3034af7fd636ef4f831d4f229c52b50
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2024-08-15 14:57:33 +0530

    Time: 4 ms (32.51%), Space: 8.8 MB (38.43%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        string ans="";
+        unordered_map<char,int>mpp;
+        for(auto it:s)
+        {
+            mpp[it]++;
+        }
+
+        priority_queue<pair<int,char>>pq;
+        for(auto it:mpp)
+        {
+            pq.push(make_pair(it.second,it.first));
+        }
+        while(pq.size()>1)
+        {
+            auto top1 =pq.top();
+            pq.pop();
+            auto top2 = pq.top();
+            pq.pop();
+
+            ans+=top1.second;
+            ans+=top2.second;
+
+            top1.first-=1;
+            top2.first-=1;
+
+            if(top1.first>0)
+            {
+                pq.push(top1);
+            }
+            if(top2.first>0)
+            {
+                pq.push(top2);
+            }
+
+        }
+
+        if(!pq.empty())
+        {
+            if(pq.top().first>1)
+            {
+                return "";
+            }
+            else{
+                ans+=pq.top().second;
+            }
+        }
+    return ans;
+    }
+};
