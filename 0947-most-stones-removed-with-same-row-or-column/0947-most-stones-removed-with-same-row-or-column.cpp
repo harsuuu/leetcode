258acace0dbb84e72ec7ab8d6f3ca9147a12c217
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2024-08-30 00:09:37 +0530

    Time: 209 ms (5.07%), Space: 18 MB (85.8%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>&stones,int index,vector<bool>&visited)
+    {
+        visited[index]=true;
+        
+        for(int i=0;i<stones.size();i++)
+        {
+            int r = stones[index][0];
+            int c = stones[index][1];
+            if((visited[i]==false) && (stones[i][0]==r || stones[i][1]==c))
+            {
+                dfs(stones,i,visited);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+
+        vector<bool>visited(n,false);
+        int group=0;
+        for(int i=0;i<n;i++)
+        {
+            if(visited[i]==true)
+            {
+                continue;
+            }
+            dfs(stones,i,visited);
+            group++;
+        }
+
+
+    return n - group;
+    }
+};
