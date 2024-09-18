7154b886a5a9615b25387e6e2e5c4efc7ce6263f
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2024-09-18 15:26:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int a = stoi(num1);
+        int b = stoi(num2);
+
+        string c = to_string(a+b);
+    return c;
+    }
+};
