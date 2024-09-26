a6774f1a23ff5b1deb1b42e092c6da6fde222c30
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2024-09-26 15:11:07 +0530

    Time: 51 ms (28.62%), Space: 51.1 MB (42.2%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxIndex = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(i>maxIndex)  return false;
+            maxIndex=max(maxIndex,i+nums[i]);
+        }
+    return true;
+    }
+};
