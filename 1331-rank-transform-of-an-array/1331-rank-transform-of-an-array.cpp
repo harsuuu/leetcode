960558c528889e526b400a3e18865f1dab4a8048
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2024-10-02 09:35:28 +0530

    Time: 100 ms (31.14%), Space: 49.7 MB (19.15%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-       priority_queue<int,vector<int>,greater<int>> minheap;
-        for(auto it:arr){
-            minheap.push(it);
+        unordered_map<int, int> mp; //number -> rank
+        set<int> st(begin(arr), end(arr)); //ordered set keeps them in sorted order
+
+        int rank = 1;
+        for(auto &num : st) {
+            mp[num] = rank;
+            rank++;
         }
-        unordered_map<int,int>mpp;
-        int rank=1;
-        while(minheap.size()!=0){
-            
-            int data=minheap.top();
-            minheap.pop();
-            if(mpp.find(data)==mpp.end()){
-                //basically nhi mila.
-                mpp[data]=rank;
-                rank++;
-            }
+
+        for(int i = 0; i < arr.size(); i++) {
+            arr[i] = mp[arr[i]];
         }
-        int n=arr.size();
-        vector<int> ans(n);
-        for(int i=0;i<arr.size();i++){
-            ans[i]=mpp[arr[i]];
-        }
-        return ans;
+
+        return arr;
     }
 };
