8f83c891f617e1649abec3af1a98195e8d8a9898
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2024-10-13 23:57:05 +0530

    Time: 91 ms (11.42%), Space: 27.3 MB (12.6%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int k = nums.size();
+
+        //{element, listIdx, idx} --> vector<int>{a, b, c}
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        int maxEl = INT_MIN;
+        for(int i = 0; i < k; i++) {
+            pq.push({nums[i][0], i, 0});
+            maxEl = max(maxEl, nums[i][0]);
+        }
+
+        vector<int> resultRange = {-1000000, 1000000};
+
+        while(!pq.empty()) {
+            vector<int> curr = pq.top();
+            pq.pop();
+
+            int minEl = curr[0];
+            int listIdx = curr[1];
+            int idx     = curr[2];
+
+            if(maxEl - minEl < resultRange[1] -  resultRange[0]) {
+                resultRange[0] = minEl;
+                resultRange[1] = maxEl;
+            }
+
+            //decrease the range for minimums ide
+            if(idx+1 < nums[listIdx].size()) {
+                int nextElement = nums[listIdx][idx+1];
+                pq.push({nextElement, listIdx, idx+1});
+                maxEl = max(maxEl, nextElement);
+            } else {
+                break;
+            }
+        }
+
+        return resultRange;
+    }
+};
