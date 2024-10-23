92d17a58101b4d4ade99ca6202a3b9a462a49de7
Harsh Joshi <107912873+harsuuu@users.noreply.github.com>
2024-10-23 21:57:17 +0530

    Time: 15 ms (97.55%), Space: 324.1 MB (24.95%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        if(root == NULL)
+            return root;
+
+        queue<TreeNode*> que;
+        que.push(root);
+        vector<int> levelSum;
+
+        //Step-1 (find sum of each level and store in levelSum array)
+        while(!que.empty()) {
+            int currLevelSum = 0;
+            int n = que.size();
+            while(n--) {
+                TreeNode* curr = que.front();
+                que.pop();
+                currLevelSum += curr->val;
+                if(curr->left) {
+                    que.push(curr->left);
+                }
+                if(curr->right) {
+                    que.push(curr->right);
+                }
+            }
+
+            levelSum.push_back(currLevelSum);
+        }
+
+        //Step-2 (update each node value with cousin sum)
+        que.push(root);
+        root->val = 0; //root has not cousins
+        int i = 1;
+        while(!que.empty()) {
+            int n = que.size(); 
+
+            while(n--) {
+                TreeNode* curr = que.front();
+                que.pop();
+
+                int siblingSum = curr->left != NULL ? curr->left->val : 0;
+                siblingSum += curr->right != NULL ? curr->right->val : 0;
+
+                if(curr->left) {
+                    curr->left->val = levelSum[i] - siblingSum;
+                    que.push(curr->left);
+                }
+                if(curr->right) {
+                    curr->right->val = levelSum[i] - siblingSum;
+                    que.push(curr->right);
+                }
+            }
+            i++;
+        }
+
+        return root;
+
+    }
+};
